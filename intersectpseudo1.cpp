79c95d25cd89148dac1ad53d06467d8053bee1f5
kammitama5 <krystal.maughan@gmail.com>
2017-03-07 15:15:30 -0800

    wip start

---
+++ intersectpseudo1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+//pseudocode for function intersect function 
+
+template < class intersectvar > 
+//takes any type of var 
+intersectvar intersect(intersectvar a, intersectvar b){
+	//intersection function
+	return;
+}
+int main(){
+	int array[7] = { 1, 2, 3, 4, 5, 1, 2 };
+	int array2[7] = { 2, 3, 4, 5, 1, 2 };
+
+	// test with char array, intarray, doublearray
+
+	//print out array
+	//cout << intersect(a, b) << endl;
+
+	system("PAUSE");
+	return 0;
+}
