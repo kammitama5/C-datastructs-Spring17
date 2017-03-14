5b671fe916537c9f31a65e1d763f6a102df17c6a
kammitama5 <krystal.maughan@gmail.com>
2017-03-14 08:38:40 -0700

    BinarySearchtemplate with pre and post cond input

---
+++ binarySearchtemplate.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+
+using namespace std;
+
+int main(){
+	// Binary Search: Rewrite the binary search algorithm from Ch 2 as a template
+	// (so this one is required to be a template). So it should search 
+	// int arrays, double arrays, string arrays - basically any type of array
+	// where the <and> operators are defined 
+
+	// Pre condition -> can take either int array, double array, string array 
+	// outputs whether found or not found or index of search (or else -1; ie not found)
+
+	// Post-condition -> returns an int that is the power a ^ n for the values entered 
+	
+	system("PAUSE");
+	return 0;
+}
+
