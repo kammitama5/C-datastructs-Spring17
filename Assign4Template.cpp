729d05c12fe1dbdfedff2a958380487c15f46304
kammitama5 <krystal.maughan@gmail.com>
2017-03-19 10:53:33 -0700

    thought process so far
    I totally thought I had to run an entire thing for this. Oh well

---
+++ Assign4Template.cpp
@@ -0,0 +1,27 @@
+// 8.  This should be a function we could add to the ArrayBag
+// not the recursive or resizable one the regular one) that would remove
+// a random entry from the bag. So it removes any random entry.
+
+// remove random entry from bag
+
+
+// take Array Bag
+// change to vector
+//check if bag is full or empty
+//if bag is empty, return "bag is empty"
+//else, check size of bag
+//check if it contains a random item
+// if it is, remove that item 
+// validate the new size of bag is one less than before (ie item was removed)
+
+
+
+//9. This should be a function we could add to the ArrayBag
+// not the recursive or resizable one the regular one). It should be 
+// a constructor that takes an array as an argument and creates an ArrayBag.
+// You can assume the arraysize is less than the maximum size allowed for the bag
+
+// creates ArrayBag from array
+// check if bag is empty
+// if bag is not empty, check size of bag
+// std vector to array -> for each item...create item in bag
