baf201ea5cdf25ab22f15ba9b7da6add42177b2d
kammitama5 <krystal.maughan@gmail.com>
2017-04-03 11:28:14 -0700

    temp questions

---
+++ HW5temp.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+/* #2 Revise the add method of the LinkedBag class so that the new node is inserted
+      at the end of the linked chain instead of the beginning. Pseudocode is ok but if possible
+      try to write C++ code
+*/
+
+/* #3 Revise the getCurrentSize member function of LinkedBag so that it would work if
+	  the LinkedBag didn't have the member variable itemCount. You only have to do part a
+	  the iterative solution. Pseudocode is ok on this one but again try the C++.	
+*/
+
+/*
+  #10 Note: you only need to define the class for the Node (not for the list) just the .h file
+	  is fine. This doesn't have to be perfect just give good ideas on how you would structure
+	  the Node class for a doubly linked list.
+*/
+
+
+
+
+	return 0;
+}
+
