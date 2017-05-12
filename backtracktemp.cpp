7e8641f4ec5ea558764d6113e48e040b5492696a
kammitama5 <krystal.maughan@gmail.com>
2017-05-12 12:11:36 -0700

    backtrack start

---
+++ backtracktemp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  
+  int row;
+  
+  cout << "Please enter the size of the board for which you'd like to solve: " << endl;
+  cin >> row;
+  int col = row;
+  
+  cout << "Row: " << row << " Column: " << col << endl;
+  
+  
+  
+  
+  
+  
+  return 0;
+}
+
